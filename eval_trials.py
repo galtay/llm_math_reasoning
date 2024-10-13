363aa80124097a94e82b1e5ed1251cefb7fcbdc0
Gabriel Altay <gabriel.altay@gmail.com>
2024-10-13 19:49:23 -0400

    new

--- eval_trials.py
+++ eval_trials.py
@@ -1,8 +1,8 @@
 import json
 
 fnames = [
-    "meta-llama/Llama-3.1-8B-Instruct/output.json",
-    "meta-llama/Llama-3.2-3B-Instruct/output.json",
+    "output/meta-llama/Llama-3.1-8B-Instruct/output.json",
+    "output/meta-llama/Llama-3.2-3B-Instruct/output.json",
 ]
 
 
