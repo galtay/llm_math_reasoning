8b220613076ff16291cdc97da1320ef3b6db6e64
Gabriel Altay <gabriel.altay@gmail.com>
2024-10-13 20:09:31 -0400

    new

--- eval_trials.py
+++ eval_trials.py
@@ -5,13 +5,21 @@ fnames = [
     "output/meta-llama/Llama-3.2-3B-Instruct/output.json",
 ]
 
-
+outputs = []
 for fname in fnames:
     with open(fname, "r") as fp:
-        outputs = json.load(fp)
+        out1 = json.load(fp)
     print(fname)
-    corrects = [el["correct"] for el in outputs]
+    pieces = fname.split("/")
+    model = pieces[-2]
+    corrects = [el["correct"] for el in out1]
     acc = sum(corrects) / len(corrects)
+    for el in out1:
+        el["model"] = model
     print("num samples: ", len(corrects))
     print("accuracy: ", acc)
     print()
+    outputs.extend(out1)
+
+with open("combined_outputs.json", "w") as fp:
+    json.dump(outputs, fp)
